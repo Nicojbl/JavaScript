13c7f4d90518941724237f1246a72d48757d84cd
Nicojbl <bassonicolasnjbl@gmail.com>
2022-09-06 20:38:09 -0300

    suma de productos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const IVA = 1.22
+let seleccionado = 1
+let total = 0
 
 class Producto {
     constructor(nombre, precio, stock) {
@@ -14,26 +16,38 @@ class Producto {
     }
 }
 
+const carrito = []
+
 const producto1 = new Producto("Monopatin", 500, 50)
 const producto2 = new Producto("Moto", 1000, 35)
 const producto3 = new Producto("Bici", 300, 28)
 
-function carrito(seleccionado) {
+function agregarAlCarrito(seleccionado) {
     switch(seleccionado) {
         case 1:
+            carrito.push(producto1)
             console.log("El producto sleccionado es " + producto1.nombre + " con un precio de: $ "+ producto1.precioConIva())
             break;
 
         case 2:
+            carrito.push(producto2)
             console.log("El producto sleccionado es " + producto2.nombre + " con un precio de: $ "+ producto2.precioConIva())
             break;
         
         case 3:
+            carrito.push(producto3)
             console.log("El producto sleccionado es " + producto3.nombre + " con un precio de: $ "+ producto3.precioConIva())
             break;
-
     }
+
 }
 
-let seleccionado = parseInt(prompt("Cuales deseas comprar? \n 1-Monopatin \n 2-Moto \n 3-bici?"));
-carrito(seleccionado);
+while(seleccionado != 0){
+    seleccionado = parseInt(prompt("Cuales deseas comprar? \n 1-Monopatin \n 2-Moto \n 3-bici \n 0-Salir"));
+    agregarAlCarrito(seleccionado);
+}
+
+for (item of carrito){
+    total += item.precioConIva()
+}
+console.log(total)
